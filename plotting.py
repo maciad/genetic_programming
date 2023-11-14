8331b4b4b39241a687ffbb637df72f159b74be74
Maciej Adamus <adamus.mac@gmail.com>
2023-11-15 00:02:46 +0100

    results of symbolic regression

--- plotting.py
+++ plotting.py
@@ -2,6 +2,8 @@ from sympy import symbols, sympify
 import matplotlib.pyplot as plt
 import glob
 import numpy as np
+import os
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def func1(x):
@@ -23,21 +25,23 @@ def func6(x, y):
     return x**2 + 3*x*y - 7*y + 1
 
 files_to_run = []
-# all files in data/zad1, data/zad4, data/zad6
-# for filename in glob.glob('data/zad1/*'):
-#     files_to_run.append(filename)
-# for filename in glob.glob('data/zad4/*'):
-#     files_to_run.append(filename)
-# for filename in glob.glob('data/zad6/*'):
-#     files_to_run.append(filename)
-files_to_run.append('results/zad1/-1000_1000.txt')
+for filename in glob.glob('results/zad1/*.txt'):
+    files_to_run.append(filename)
+for filename in glob.glob('results/zad4/*.txt'):
+    files_to_run.append(filename)
+for filename in glob.glob('results/zad6/*.txt'):
+    files_to_run.append(filename)
 
 for file in files_to_run:
-    x_min, x_max = file.split('/')[-1].split('\\')[-1].split('.')[0].split('_')
+    savefile = file.replace('.txt', '.png')
+    # x_min, x_max = file.split('/')[-1].split('\\')[-1].split('.')[0].split('_')
+    x_min, x_max = os.path.splitext(os.path.basename(file))[0].split('_')
+    print(x_min, x_max)
     x_min = int(x_min)
     x_max = int(x_max)
 
-    problem = file.split('/')[1][-1]
+    # problem = file.split('/')[1][-1]
+    problem = file[11]
     func = None
     if problem == '1':
         func = func1
@@ -52,40 +56,58 @@ for file in files_to_run:
     elif problem == '6':
         func = func6
 
-    # Definicja zmiennych symboli
-    X1, X2 = symbols('X1 X2')
-    
-    # Wyrażenie matematyczne jako string
-    with open ('results/zad1/-1000_1000.txt', 'r') as f:
+    with open(file, 'r') as f:
         expression = f.read().strip()
+        expr = sympify(expression)
 
-    # Parsowanie wyrażenia do obiektu sympy
-    expr = sympify(expression)
+    if problem in ['1', '2', '3']:
+        X1 = symbols('X1')
+        
+        X = np.linspace(x_min, x_max, 100)
+        result = np.zeros(len(X))
+        original_function = np.zeros(len(X))
+        for i in range(len(X)):
+            result[i] = expr.subs({X1: X[i]})
+            original_function[i] = func(X[i])
 
-    X = np.linspace(x_min, x_max, 100)
-    y = np.zeros(len(X))
-    original_function = np.zeros(len(X))
-    for i in range(len(X)):
-        y[i] = expr.subs({X1: X[i], X2: X[i]})
-        original_function[i] = func(X[i])
-    # Wyświetlenie wykresu
-
-    plt.plot(X, original_function, label='original function', color='blue')
-    plt.plot(X, y, label='fitted function', color='orange', linestyle='dashed')
-    plt.xlabel('X')  # Etykieta osi x
-    plt.ylabel('Y')  # Etykieta osi y
-    plt.title('Wykres funkcji')  # Tytuł wykresu
-    plt.grid(True)  # Włączenie siatki na wykresie
-    plt.savefig(f'results/zad1/-1000_1000.png')
-    # plt.show()  # Wyświetlenie wykresu
+        plt.plot(X, original_function, label='original function', color='blue')
+        plt.plot(X, result, label='fitted function', color='orange', linestyle='dashed')
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.title('Wykres funkcji')
+        plt.grid(True)
+        plt.legend()
+        plt.savefig(savefile)
+        plt.clf()  # Wyczyszczenie rysunku
 
 
+    elif problem in ['4', '5', '6']:
+        X1, X2 = symbols('X1 X2')
 
-# # Przykładowe wartości dla zmiennych X1 i X2
-# X1_val = 2
-# X2_val = 10
+        x = np.linspace(x_min, x_max, 100)
+        y = np.linspace(x_min, x_max, 100)
+        X, Y = np.meshgrid(x, y)
 
-# # Podstawienie wartości zmiennych i obliczenie wyniku
-# result = expr.subs({X1: X1_val, X2: X2_val})
+        original_function = func(X, Y)
 
-# print(f"Wynik wyrażenia dla X1={X1_val} i X2={X2_val}: {result}")
+        result = np.zeros((len(x), len(y)))
+        for i in range(len(x)):
+            for j in range(len(y)):
+                result[i][j] = expr.subs({X1: x[i], X2: y[j]})
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot_surface(X, Y, original_function, label='original function', color='blue')
+        ax.plot_surface(X, Y, result, label='fitted function', color='orange', linestyle='dashed')
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        ax.set_title('Wykres funkcji')
+        # ax.legend()
+        plt.savefig(savefile)
+        plt.clf()
+
+
+
+    
+
